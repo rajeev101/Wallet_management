9a59f8a885041aaff7e2f022c6443ce56a6fcb47
ShubhamCodingZone <shub0834cs231187@gmail.com>
2026-07-13 15:37:04 +0530

    Delete admin directory

--- admin/src/components/common/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ text, type = "submit", disabled = false }) {
-  return (
-    <button className="primary-btn" type={type} disabled={disabled}>
-      {text}
-    </button>
-  );
-}
-
-export default Button;
